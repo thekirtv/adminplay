d804ffc226534f33887b4225c2ddea022fd80608
thekirtv <149680777+thekirtv@users.noreply.github.com>
2024-04-20 16:12:04 +0300

    Delete app directory

--- app/api/config.js
+++
@@ -1,8 +0,0 @@
-export const BASE_URL = 'https://api-code-2.practicum-team.ru';
-
-export const endpoints = {
-  games: `${BASE_URL}/games`,
-  auth: `${BASE_URL}/auth/local`,
-  me: `${BASE_URL}/users/me`,
-};
-
